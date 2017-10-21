006fbda70aa6b30ab9a88a9c0d8f9f5ced6f3520
Torben <Torben@PC-Torben>
2017-10-21 13:53:15 +0200

    21.10

--- src/blatt_02/aufgabe_1.scala
+++ src/blatt_02/aufgabe_1.scala
@@ -1,4 +1,36 @@
-def main (args: Array[String]) Unit= (
+package blatt_02
 
-	val a : Boolean = false
-	val b : Boolean = true
+object aufgabe_1 {
+
+def main (args: Array[String]): Unit = {
+  
+ 	val a : Boolean = false
+	val b : Boolean = false
+
+	
+	
+	//Teilaufgabe b
+	def NAND (a : Boolean,b : Boolean) : Boolean = !(a&&b)
+	
+	//Teilaufgabe c
+	def NOT (a : Boolean) : Boolean = NAND(a,a)
+	
+	//Teilaufgabe d
+	def AND (a : Boolean,b : Boolean) : Boolean = NOT(NAND(a,b))
+	
+	//Teilaufgabe e
+	def OR (a : Boolean,b : Boolean) : Boolean = NOT(AND(NOT(a),NOT(b)))
+	def NOR (a : Boolean,b : Boolean) : Boolean = NOT(OR(a,b))
+	
+	//Teilaufgabe f
+	def XOR (a : Boolean,b : Boolean) : Boolean = OR(AND(a,b),NOR(a,b))
+	
+	
+	println ("NAND 	= " + NAND(a,b)) 
+	println ("NOT 	= " + NOT(a))
+  println ("AND 	= " + AND(a,b))
+  println ("OR 	= " + OR(a,b))
+  println ("NOR 	= " + NOR(a,b)) 
+  println ("XOR	= " + XOR(a,b))  }
+	
+}
