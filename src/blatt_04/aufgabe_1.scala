3eabeaa820bc7f7eb00cba2f479b9fea96d00f3a
Torben335 <32965336+Torben335@users.noreply.github.com>
2017-11-06 15:04:46 +0100

    Update aufgabe_1.scala

--- src/blatt_04/aufgabe_1.scala
+++ src/blatt_04/aufgabe_1.scala
@@ -5,16 +5,18 @@ object aufgabe_1 {
 def main (args: Array[String]): Unit = {
 		
   
-	//Teilaufgabe a
+	//Teilaufgabe a (nur für sortierte Listen mit Elementen, die nur einmal vorkommen --> ist so gewollt)
 	val L1 = List(5,1,5,17)
 	val L2 = List(3,5,18)
 	
 	
-	/*def union(L1:List[Int],L2:List[Int]) : List[Int] = (L1,L2) match {
+	def union(L1:List[Int],L2:List[Int]) : List[Int] = (L1,L2) match {
 		
+	case(List(),_) => L2
+	case(_,List()) => L1
 	case (x::xRest, y::yRest) =>
-		if (x < y) x::union(xRest, y::yRest)
-		else if (y < x) y::union(yRest, x::xRest)
+		if (x < y) x::union(xRest, L2)
+		else if (y < x) y::union(L1, yRest)
 		else x::union(yRest, xRest)
 	
 	case (_,_) => L1 ::: L2 	
@@ -26,13 +28,13 @@ def main (args: Array[String]): Unit = {
 	
 	
 	println(union(L1,L2)) 
-	*/
-
+	
+/*
 	
 	println(()
 	
 	
-	/*Teilaufgabe b
+	/*Teilaufgabe b --> gucken wann a und b gleich sind und dann in eine Liste packen
 	def intersect (L1:List[Int],List2[Int]):List[Int] = (L1,L2) match {
 	  
 	 case (x::xRest, y::yRest) =>
