93c89c614bf6e01c148b6265746d543df7249cea
Torben335 <32965336+Torben335@users.noreply.github.com>
2017-11-06 14:40:02 +0100

    Update aufgabe_4.scala

--- src/blatt_03/aufgabe_4.scala
+++ src/blatt_03/aufgabe_4.scala
@@ -6,8 +6,10 @@ def main (args: Array[String]): Unit = {
 		
   
   
-	//Teilaufgabe a ((Vorname, Nachname), Email, Tel)
+	//Teilaufgabe a ((Vorname, Nachname), Email, Tel) --> wennn es zb mehrere Emails gibt List[String]
 	type Kontakt = ((String,String),String,String)
+	
+	type Kontaktliste = List[Kontakt]
 		
 	val Torben : Kontakt = (("Torben", "Zimmermann"), "Torben.Z@gmx.de", "015161246525")
 	val Mama : Kontakt = (("Birgit", "Lausen"), "B.Zimmermann4@gmx.de", "01744564945")
@@ -16,7 +18,14 @@ def main (args: Array[String]): Unit = {
 	
 		println(Kontaktliste) 
 	
-	//Teilaufgabe b
+	//Teilaufgabe b (k._2 --> Geht in die Liste k rein und guckt auf die zweite Stelle)
+	def eMailKontaktliste (l:Kontaktliste):List[String] = {
+		def eMail (k:Kontakt):List[String] = {
+			k._2 }
+		match
+		    case List()=>List()
+		    case x::xs =>eMail(x):::eMailKontaktliste(xs)
+	
   val eMailAdressen = List[String](Torben._2, Mama._2)
 		
 	println(eMailAdressen)
